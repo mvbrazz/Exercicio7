3ed76fb91d38d485473608b90253edeff107a8be
Marcus Vinicus <mvbrazz@hotmail.com>
2021-10-26 21:44:54 -0300

    Atividade 7 Ex 1

--- exercicio.js
+++ exercicio.js
@@ -23,6 +23,22 @@ console.log(apps[0]);
 
 // EXERCICIO 1: use reduce() para calcular o numero total de installs para todas as apps.
 
+const contador = apps.length;
+let total = 0;
+
+const aux = apps.reduce((ant, atual) => {
+/*apps[0].installs*/
+
+    if(contador > ant){
+        total = total + atual.installs;
+        return contador-1;
+    }
+    else
+        return ant;
+}, 0);
+
+console.log('O total de installs de todas as apps são de:',total);
+
 // EXERCICIO 2: use filter() para selecionar somente apps com score maior que quatro (> 4) 
 
 
